1d5933fc2f712ed959d683eb853a9cc48530c91c
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2023-04-13 01:23:54 +0400

    feat: add new class

--- TeacherView.java
+++ TeacherView.java
@@ -5,11 +5,11 @@ import ru.gb.oseminar.view.UserView;
 import java.util.List;
 import java.util.logging.Logger;
 
-public class TeacherView implements UserView<Teacher>{
+public class TeacherView<T extends Teacher> {
     Logger logger = Logger.getLogger(TeacherView.class.getName());
 
 
-    public Teacher sendOnConsole(List<User> list) {
-        return (Teacher) list.get(0);
+    public Teacher sendOnConsole(List<Teacher> list) {
+        return list.get(0);
     }
 }
